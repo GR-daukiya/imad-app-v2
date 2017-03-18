10aeb16a89a75734bcea4e526846692568b8a7b5
GR-daukiya <gangaramdaukiya1996@gmail.com>
2017-03-18 23:21:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/article-one',function(req,res){res.req('ram ram-1');});
+
+app.get('/article-three',function(req,res){res.req('ram ram-3');});
+
+app.get('/article-two',function(req,res){res.req('ram ram-2');});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
